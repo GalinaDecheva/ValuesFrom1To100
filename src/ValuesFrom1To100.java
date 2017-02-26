fd26d1f34153dedd5d791614b0e26680b5b1a826
Galina Decheva <de_galina@abv.bg>
2017-02-26 21:34:55 +0200

    Ready

---
+++ src/ValuesFrom1To100.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ValuesFrom1To100 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+	
+		
+		for (int i = 1; i < 101; i ++){
+			System.out.println(i);
+		}
+		input.close();
+	}
+
+}
